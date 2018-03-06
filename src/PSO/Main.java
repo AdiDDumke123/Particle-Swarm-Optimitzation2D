0aec99e5791c3a96197cd31155593f3f30bdc502
tim <kuehnleintim@gmail.com>
2018-03-06 21:41:43 +0100

    only inputs: swarm size & iteration

--- src/PSO/Main.java
+++ src/PSO/Main.java
@@ -5,27 +5,17 @@ import java.util.Scanner;
 public class Main {
 
     public static void main (String[] args) {
-        menu(true);
+        menu();
     }
 
-    private static void menu (boolean flag) {
+    private static void menu () {
         Swarm swarm;
         int particles, epochs;
-        double inertia, cognitive, social;
 
         particles = getUserInt("Particles: ");
         epochs = getUserInt("Epochs:    ");
 
-        if (flag) {
-            inertia = getUserDouble("Inertia:   ");
-            cognitive = getUserDouble("Cognitive: ");
-            social = getUserDouble("Social:    ");
-            swarm = new Swarm(particles, epochs, inertia, cognitive, social);
-        } else {
-            swarm = new Swarm(particles, epochs);
-
-        }
-
+        swarm = new Swarm(particles, epochs);
         swarm.run();
     }
 
@@ -37,28 +27,6 @@ public class Main {
 
             if (sc.hasNextInt()) {
                 input = sc.nextInt();
-
-                if (input <= 0) {
-                    System.out.println("Number must be positive.");
-                } else {
-                    break;
-                }
-
-            } else {
-                System.out.println("Invalid input.");
-            }
-        }
-        return input;
-    }
-
-    private static double getUserDouble (String msg) {
-        double input;
-        while (true) {
-            Scanner sc = new Scanner(System.in);
-            System.out.print(msg);
-
-            if (sc.hasNextDouble()) {
-                input = sc.nextDouble();
 
                 if (input <= 0) {
                     System.out.println("Number must be positive.");
